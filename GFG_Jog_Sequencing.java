93164b05680f6eeda8b982919e707386d3890008
Avanish Raj Singh <99349020+avanishraj@users.noreply.github.com>
2023-04-17 21:56:41 +0530

    Add files via upload

---
+++ GFG_Jog_Sequencing.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    int[] JobScheduling(Job arr[], int n)
+    {
+        int c=0,totalprofit=0;
+        Arrays.sort(arr,(x,y)->x.deadline-y.deadline);
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            if(arr[i].deadline>pq.size())pq.add(arr[i].profit);
+            else if(arr[i].deadline==pq.size()){
+                if(arr[i].profit>pq.peek()){
+                    pq.poll();
+                    pq.add(arr[i].profit);
+                }
+            }
+        }
+        int ans[]=new int[2];
+        c=pq.size();
+        while(!pq.isEmpty())totalprofit+=pq.poll();
+        ans[0]=c;
+        ans[1]=totalprofit;
+        return ans;
+    }
+}
